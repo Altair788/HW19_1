c86bd94b0752e90e1ecc021126bad97d4620cdbe
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-08-12 19:32:32 +0300

    fix web: add remote url

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 hostName = "localhost"
@@ -9,19 +10,26 @@ class MyServer(BaseHTTPRequestHandler):
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
-    filename = "web.html"
+    local_filename = "web.html"
+    remote_filename_url = "https://raw.githubusercontent.com/Altair788/HW19_1/develop/remote_web.html"
 
     def get_context_data(self):
-        with open(self.filename, "r", encoding="utf-8") as f:
+        with open(self.local_filename, "r", encoding="utf-8") as f:
             context = f.read()
         return context
+
+    def get_context_data_from_git(self):
+        response = requests.get(self.remote_filename_url)
+        return response.text if response.status_code == 200 else "<h1>Error loading page</h1>"
 
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes(self.get_context_data(), "utf-8"))
+        #  локальная версия
+        # self.wfile.write(bytes(self.get_context_data(), "utf-8"))
+        self.wfile.write(bytes(self.get_context_data_from_git(), "utf-8"))
 
 
 if __name__ == "__main__":
